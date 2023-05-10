4aad5412cc7d5fab6ba3374615cc6312bb39892c
TheHaterNoob <HATERNOOBLOL@gmail.com>
2023-05-09 18:51:12 -0600

    First Push

--- TheGame/Source.cpp
+++ TheGame/Source.cpp
@@ -1,24 +1,42 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 int main()
 {
-    // Create the window
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Platform Game");
+    sf::RenderWindow window(sf::VideoMode(1366, 768), "THE GAME");
 
-    // Load player texture
-    sf::Texture playerTexture;
-    if (!playerTexture.loadFromFile("player.png")) {
-        // Error loading texture
+
+    std::vector<sf::Texture> walkingFrames;
+    for (int i = 1; i <= 2; i++) {
+        sf::Texture frame;
+        if (!frame.loadFromFile("player" + std::to_string(i) + ".png")) {
+     
+            std::cerr << "Error loading frame " << i << std::endl;
+        }
+        else {
+            walkingFrames.push_back(frame);
+        }
+    }
+
+    if (walkingFrames.empty()) {
         return -1;
     }
 
-    // Create player sprite
-    sf::Sprite player(playerTexture);
-    player.setPosition(400, 300);
+    sf::Clock animationTimer;
+    float frameTime = 0.4f;
 
+    sf::Sprite player(walkingFrames[0]);
+    player.setPosition(683, 384);
+
+    sf::Texture platformTexture;
+    if (!platformTexture.loadFromFile("platform.png")) {
+        return -1;
+    }
+    sf::Sprite platform(platformTexture);
+    platform.setPosition(683, 484);
+    int currentFrame = 0;
     while (window.isOpen())
-    {
-        // Process events
+    {  
         sf::Event event;
         while (window.pollEvent(event))
         {
@@ -26,24 +44,26 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-
-        // Move the player sprite with arrow keys
+        if (player.getGlobalBounds().intersects(platform.getGlobalBounds())) {
+            
+            player.setPosition(player.getPosition().x, platform.getPosition().y - player.getGlobalBounds().height);
+        }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-            player.move(-1, 0);
+            player.move(-0.4, 0);
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-            player.move(1, 0);
+            player.move(0.4, 0);
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-            player.move(0, -1);
+            player.move(0, -0.4);
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-            player.move(0, 1);
-
-        // Clear screen
+            player.move(0, 0.4);
+        if (animationTimer.getElapsedTime().asSeconds() >= frameTime) {
+            currentFrame = (currentFrame + 1) % walkingFrames.size(); 
+            player.setTexture(walkingFrames[currentFrame]);
+            animationTimer.restart();
+        }
         window.clear(sf::Color::Blue);
-
-        // Draw game objects
         window.draw(player);
-
-        // Update the window
+        window.draw(platform);
         window.display();
     }
 
